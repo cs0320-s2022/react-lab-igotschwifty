0f30310af55f43b9b84e29e2f433bc9c644dca69
igotschwifty <84202966+igotschwifty@users.noreply.github.com>
2022-03-22 12:23:20 -0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Horoscope from './Horoscope';
+import TextBox from "./TextBox";
+
+function App() {
+  return (
+      <div className='App'>
+          <Horoscope/>
+      </div>
+  );
+}
+
+export default App;
